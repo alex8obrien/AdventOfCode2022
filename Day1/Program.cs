ec1c60002123478e12c727121398efc374a7b0c0
Alex O'Brien <alex8obrien@gmail.com>
2022-12-13 20:20:28 +0000

    Initial Commit

---
+++ Day1/Program.cs
@@ -0,0 +1,32 @@
+﻿using Lib;
+
+namespace Day1
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            int max = 0, sum = 0;
+            string input = StdInp.Input("Enter the number of calories each ration pack contains\nWhen starting with a new elf leave blank line");
+            List<int> calories = new List<int>();
+
+            while (Convert.ToInt32(input) != -1)
+            {
+                if (input == string.Empty)
+                {
+                    calories.Add(-1);
+                    sum = 0;
+                }
+                else
+                {
+                    calories.Add(Convert.ToInt32(input));
+                    sum += Convert.ToInt32(input);
+                }
+            }
+
+            foreach (int i in calories)
+            { Console.WriteLine(i); }
+
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,13 @@
+﻿using CommonsLibrary.StdInp;
+
+namespace Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StdInp.Input("asdads");
+            
+        }
+    }
+}
